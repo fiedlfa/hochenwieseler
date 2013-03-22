8de949ad19eef959bb2a25734488c2738566a313
FF-2012 <FF-2012@192.168.2.105>
2013-03-22 12:31:08 +0100

    Die BlueJ daten importiert ;)

---
+++ test/src/github/hochenwieseler/Integer_MinValue.java
@@ -0,0 +1,67 @@
+﻿package github.hochenwieseler;import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+/**
+ * Ein Kurzer Berechner für integer Minvalue Werte ;)
+ * 
+ * @author Fabian Fiedler
+ * @version 22.03.2013
+ */
+public class Integer_MinValue
+{
+    private CyclicBarrier barrier;
+    private int _laenge;
+    /**
+     * Ein Kurzer Berechner für integer Minvalue Werte ;). Gibt die Werte auf der Konsole aus     
+     * Analog zum intStarter.
+     * Sinvoll ist eine länge von 7. 
+     * dann kommen zwar nur wenige strings raus aber 8 dauert noch ewig ;)
+     * @param laenge die Ziellänge des Strings
+     */
+    public static void main(String[] args)
+    {
+        new Integer_MinValue().berechneMinValue(args[0].charAt(0));
+    }
+    /**
+     * Ein Kurzer Berechner für integer Minvalue Werte ;). Gibt die Werte auf der Konsole aus
+     * Analog zur Main Methode
+     * @param laenge die Ziellänge des Strings
+     */
+    public static void intStarter(int i)
+    {
+        new Integer_MinValue().berechneMinValue(i);
+    }
+
+    /**
+     *  der eigentliche Starter ;)
+     *  @param laenge die Ziellänge des strings
+     *
+     */
+    public void berechneMinValue(int laenge)
+    {
+        long startzeit = System.nanoTime();
+        int anzahl_Treads = 2* 24 + 1; // alle kleinen Buchstaben alle große und der "master" selber
+        barrier = new CyclicBarrier(anzahl_Treads+1);
+        _laenge = laenge;
+        for(char i = 'a'; i <= 'z'; i++)
+        {
+            Thread t = new Thread(new hashBerechnerRunnable(i,barrier,_laenge));
+            t.start();
+        }
+        for(char i = 'A'; i <= 'Z'; i++)
+        {
+            Thread t = new Thread(new hashBerechnerRunnable(i,barrier,_laenge));
+            t.start();
+        }
+
+        try {
+            barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println((System.nanoTime() - startzeit)/1000000 + "ms");
+    }
+
+
+}
---
+++ test/src/github/hochenwieseler/hashBerechnerRunnable.java
@@ -0,0 +1,68 @@
+package github.hochenwieseler;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier; 
+/**
+ * Eine Runnable um alle Kerne der CPU via Threads möglich zu machen
+ * 
+ */
+public class hashBerechnerRunnable implements Runnable
+{
+    private int _anfangsbuchstabe;
+    CyclicBarrier _barrier;
+    int _laenge;
+    public hashBerechnerRunnable(char anfangsbuchstabe,CyclicBarrier barrier,int laenge)
+    {
+        _anfangsbuchstabe = anfangsbuchstabe;     
+        _barrier = barrier;
+        _laenge = laenge;
+    }
+
+    public void run()
+    {
+        rechner(_laenge -1, _anfangsbuchstabe * pow(31,_laenge-1),"" + (char)_anfangsbuchstabe);
+        try {
+            _barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * der eigentliche Rechner.
+     *  laenge ist die restliche laenge des Späteren Strings (beginnend mit 1)
+     *  hashValue ist der bishergerechnete Wert für den hashcode
+     *  value ist der aktuelle string der in berechnung ist.
+     */
+    private void rechner(int laenge, int hashValue,String value)
+    {
+        if(laenge > 0)
+        {
+            for (int i = 'a'; i < 'z'; i++)
+            {
+                rechner(laenge -1, hashValue + (i * pow(31,laenge-1)) , value + (char)i );
+            }
+            rechner(laenge -1, hashValue + (' ' * pow(31,laenge-1)) , value + ' ' );
+        }
+        else if (hashValue == Integer.MIN_VALUE)
+        {
+            System.out.println(value);
+        }
+
+    }
+
+    /**
+     * Berechnet x hoch y
+     */    
+    private int pow(int x, int y)
+    {
+        int a = 1;
+        for(int i = 0;i < y;i++)
+        {
+            a *= x;
+        }
+        return a;
+    }
+}
